082cfc926ceb35e5a2c0b2aa984ede30add534c5
“mayinghan” <ma.yingha@ufl.edu>
2019-08-14 13:18:07 +0800

    authrouter done, navbar under dev

--- src/component/AuthRoute/AuthRoute.js
+++ src/component/AuthRoute/AuthRoute.js
@@ -3,11 +3,60 @@ import axios from 'axios';
 import { withRouter } from 'react-router-dom';
 import { loadData } from '../../redux/user.redux';
 import { connect } from 'react-redux';
+import NavBar from '../NavBar/NavBar';
+import Login from '../../container/auth/Login';
 
+
+function Home() {
+  return <h2>Airpick homepage</h2>
+}
+
+function Register() {
+  return <h2>Register page</h2>
+}
 @withRouter
-@connect(state=>state.user, {loadDate})
+@connect(state=>state.user, {loadData})
 class AuthRoute extends React.Component {
-  componentDidMount() {
-    
+  componentWillMount() {
+
+  }
+
+  render() {
+    const isAuth = this.props.user.isAuth;
+    const navList = [
+      {
+        path: '/home',
+        text: 'Airpick',
+        key: 'home',
+        component: 'Home',
+        hide: false
+      },
+      {
+        path: '/login',
+        text: 'login',
+        key: 'login',
+        component: 'Login',
+        hide: isAuth
+      },
+      {
+        path: '/register',
+        text: 'register',
+        key: 'register',
+        component: 'Register',
+        hide: isAuth
+      },
+      {
+        text: 'logout',
+        key: 'logout',
+        hide: !isAuth
+      }
+    ];
+
+    return (
+      <Fragment>
+        <NavBar data={navList}>
+        </NavBar>
+      </Fragment>
+    )
   }
 }
---
+++ src/component/NavBar/NavBar.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+import { Menu, Icon } from 'antd';
+const SubMenu = Menu.SubMenu;
