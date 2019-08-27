6bc125042a13b7d9fc54b2cc4fc51c655700f7b2
“mayinghan” <ma.yingha@ufl.edu>
2019-08-27 14:18:14 -0400

    req form under dev, fix vul dep

--- src/component/Forms/PickreqForm.js
+++ src/component/Forms/PickreqForm.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Form, Input, Icon, Button } from 'antd';
 
-
 class PickreqForm extends React.Component {
   render() {
-    return (<h2>This is a form</h2>)
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
+      },
+    };
+    return (<h2>This is a form</h2>);
   }
 }
 
