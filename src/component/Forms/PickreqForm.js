a86375827c159c2eb48090e5d132163ba9cad034
mayinghan <mayinghan97@gmail.com>
2019-12-12 10:59:37 -0500

    add date&time fields

--- src/component/Forms/PickreqForm.js
+++ src/component/Forms/PickreqForm.js
@@ -1,8 +1,39 @@
 import React from 'react';
-import { Form, Input, Icon, Button } from 'antd';
+import { Form, Input, DatePicker, TimePicker, Button } from 'antd';
 
 class PickreqForm extends React.Component {
+  // handle submit form info
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.form.validateFields((err, fieldsValue) => {
+      if (err) {
+        return;
+      }
+
+      // Should format date value before submit.
+      const rangeValue = fieldsValue['range-picker'];
+      const rangeTimeValue = fieldsValue['range-time-picker'];
+      const values = {
+        ...fieldsValue,
+        'date-picker': fieldsValue['date-picker'].format('YYYY-MM-DD'),
+        'date-time-picker': fieldsValue['date-time-picker'].format('YYYY-MM-DD HH:mm:ss'),
+        'month-picker': fieldsValue['month-picker'].format('YYYY-MM'),
+        'range-picker': [rangeValue[0].format('YYYY-MM-DD'), rangeValue[1].format('YYYY-MM-DD')],
+        'range-time-picker': [
+          rangeTimeValue[0].format('YYYY-MM-DD HH:mm:ss'),
+          rangeTimeValue[1].format('YYYY-MM-DD HH:mm:ss'),
+        ],
+        'time-picker': fieldsValue['time-picker'].format('HH:mm:ss'),
+      };
+      console.log('Received values of form: ', values);
+      // TODO: add submit event to redux
+      // TODO: Check if time is before today
+    });
+  };
+
   render() {
+    const { getFieldDecorator } = this.props.form;
     const formItemLayout = {
       labelCol: {
         xs: { span: 24 },
@@ -13,8 +44,26 @@ class PickreqForm extends React.Component {
         sm: { span: 16 },
       },
     };
-    return (<h2>This is a form</h2>);
+
+    const requirement = {
+      rules: [{
+        type: 'object', required: true, message: 'Please select a time!'
+      }],
+    };
+
+    
+    return (
+      <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+        <Form.Item label='Date'>
+          {getFieldDecorator('date', requirement)(<DatePicker />)}
+        </Form.Item>
+        <Form.Item label='Time'>
+          {getFieldDecorator('time', requirement)(<TimePicker />)}
+        </Form.Item>
+      </Form>
+    );
   }
 }
 
-export default PickreqForm
+const WrappedPickreqForm = Form.create({ name: 'pick_time' })(PickreqForm)
+export default WrappedPickreqForm
