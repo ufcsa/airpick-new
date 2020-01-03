914f247ea2ce638aa390dba980b7e6a81c1b15bf
“mayinghan” <ma.yingha@ufl.edu>
2020-01-02 21:50:24 -0500

    add edit modal; remove publish button

--- client/component/Forms/PickModal.js
+++ client/component/Forms/PickModal.js
@@ -1,11 +1,16 @@
+/**
+ * This model is a modal that used to add NEW requests.
+ */
+
 import React from 'react';
 import { Modal, Form, Input, DatePicker, TimePicker, InputNumber, Switch } from 'antd';
+
 const { TextArea } = Input;
 
 class PickreqForm extends React.Component {
-  
+
   render() {
-    const { visible, onCancel, onSubmit } = this.props;
+    const { visible, onCancel, onSubmit } = this.props; // values/functions passed from its parent component 'RequestCenter'
     const { getFieldDecorator } = this.props.form;
     const formItemLayout = {
       labelCol: {
@@ -25,7 +30,7 @@ class PickreqForm extends React.Component {
       }],
     };
 
-    
+
     return (
       <div>
         <Modal
@@ -35,31 +40,31 @@ class PickreqForm extends React.Component {
           onOk={onSubmit}
           onCancel={onCancel}>
           <Form {...formItemLayout} onSubmit={this.handleSubmit}>
-            <Form.Item label='Publish'>
+            {/* <Form.Item label='Publish'>
               {getFieldDecorator('publish')(<Switch autoFocus></Switch>)}
-            </Form.Item>
+            </Form.Item> */}
             <Form.Item label='Date'>
-              {getFieldDecorator('date', {...requirement})
-              (<DatePicker format={dateFormat} />)}
+              {getFieldDecorator('date', { ...requirement })
+                (<DatePicker format={dateFormat} />)}
             </Form.Item>
             <Form.Item label='Time'>
-              {getFieldDecorator('time', {...requirement})
-              (<TimePicker format={timeFormat} />)}
+              {getFieldDecorator('time', { ...requirement })
+                (<TimePicker format={timeFormat} />)}
             </Form.Item>
             <Form.Item label='Airport/Location'>
-              {getFieldDecorator('airport', {...requirement})(<Input placeholder='MCO'/>)}
+              {getFieldDecorator('airport', { ...requirement })(<Input placeholder='MCO' />)}
             </Form.Item>
             <Form.Item label='Number of Carry-ons'>
-              {getFieldDecorator('carryon', {...requirement})
-              (<InputNumber min={0} max={10} placeholder={2}></InputNumber>)}
+              {getFieldDecorator('carryon', { ...requirement })
+                (<InputNumber min={0} max={10} placeholder={2}></InputNumber>)}
             </Form.Item>
             <Form.Item label='Number of large luggages'>
-              {getFieldDecorator('luggage', {...requirement})
-              (<InputNumber min={0} max={10} placeholder={2}></InputNumber>)}
+              {getFieldDecorator('luggage', { ...requirement })
+                (<InputNumber min={0} max={10} placeholder={2}></InputNumber>)}
             </Form.Item>
             <Form.Item label='Notes'>
               {getFieldDecorator('notes')
-              (<TextArea placeholder='How many people coming with you?'></TextArea>)}
+                (<TextArea placeholder='How many people coming with you?'></TextArea>)}
             </Form.Item>
           </Form>
         </Modal>
