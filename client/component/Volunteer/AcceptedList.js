49a6b6756cdbb192493fb3191ba69c6c19dc6621
Jason Chen <cyhy462702086@gmail.com>
2020-01-19 20:26:01 -0500

    Revert "changed acceptedList"
    This reverts commit 40249c270affc4a3cf2013484cd53b0066d92ae5.

--- client/component/Volunteer/AcceptedList.js
+++ client/component/Volunteer/AcceptedList.js
@@ -1,15 +1,12 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Table, Button, message } from "antd";
-import moment from "moment-timezone";
-import { loadAcceptedReq } from "../../redux/request.redux";
-
-const { Column } = Table;
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { loadAcceptedReq } from '../../redux/request.redux';
 
 export const AcceptedList = props => {
   const userState = useSelector(state => state.user);
-  const reqState = useSelector(state => state.request);
+  //const reqState = useSelector(state => state.request);
   const dispatch = useDispatch();
+
   useEffect(() => {
     if (userState.username) {
       dispatch(loadAcceptedReq(userState.username));
@@ -17,35 +14,5 @@ export const AcceptedList = props => {
   }, [dispatch, userState.username]);
 
   // TODO: iterate through reqState.acceptedList to get all accepted list
-
-  return (
-    <div>
-      <Table
-        dataSource={reqState.acceptedList}
-        loading={reqState.acceptedList}
-        pagination={false}
-        tableLayout="fixed"
-      >
-        <Column title="Name" dataIndex={["username"]} key="name" />
-        <Column
-          title="Time"
-          key="time"
-          render={(text, record) => {
-            debugger;
-            return moment(record.arrivalTime)
-              .tz("America/New_York")
-              .format("ddd, MMM Do YYYY HH:mm");
-          }}
-        ></Column>
-        <Column
-          title="Airport/Address"
-          dataIndex={["airport"]}
-          key="airport"
-        ></Column>
-        <Column title="Luggage" dataIndex={["luggage"]} key="luggage"></Column>
-        <Column title="Carryon" dataIndex={["carryon"]} key="carryon"></Column>
-        <Column title="Notes" dataIndex={["notes"]} key="notes"></Column>
-      </Table>
-    </div>
-  );
-};
+  return <h2>This is a list</h2>
+}
