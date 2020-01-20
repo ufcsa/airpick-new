f64f3c13f0ee7b719ac8210ae8a7d84fb575c508
Yinghan Ma <mayinghan97@gmail.com>
2020-01-19 19:55:03 -0500

    Revert "changed acceptedList"

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const config = require("./server/config/config");
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./server/config/config');
 const app = express();
 const Router = express.Router();
-const userRouter = require("./server/api/user-api")(Router);
-const requestRouter = require("./server/api/pickreq-api")(Router);
-const mailRouter = require("./server/api/mail-api")(Router);
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const userRouter = require('./server/api/user-api')(Router);
+const requestRouter = require('./server/api/pickreq-api')(Router);
+const mailRouter = require('./server/api/mail-api')(Router);
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-const server = require("http").Server(app);
+const server = require('http').Server(app);
 
 mongoose.connect(config.db, { useNewUrlParser: true, useFindAndModify: false });
 
@@ -17,22 +17,19 @@ var port = process.env.PORT || 5000;
 
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+})
 
-app.use("/api/user", userRouter);
-app.use("/api/requests", requestRouter);
+app.use('/api/user', userRouter);
+app.use('/api/requests', requestRouter);
 
 // for test purpose, won't use these router in production
-app.use("/api/email", mailRouter);
+app.use('/api/email', mailRouter);
 
 //bound with io server+express instead of express app itself
-server.listen(port, function() {
-  console.log("Node app starts at port ", port);
-});
+server.listen(port, function () {
+  console.log('Node app starts at port ', port)
+})
