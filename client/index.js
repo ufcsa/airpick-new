d559243c2d76048b680758f6133909b478071b42
“mayinghan” <ma.yingha@ufl.edu>
2020-02-03 00:46:52 -0500

    fixed bug when browser has no redux dev tool error

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';  //for async in redux
+import thunk from 'redux-thunk'; //for async in redux
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
@@ -10,21 +10,26 @@ import reducers from './reducers';
 import AuthRoute from './component/AuthRoute/AuthRoute';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducers, compose(
-  applyMiddleware(thunk),
-  window.devToolsExtension ? window.devToolsExtension() : () => { }
-));
+const store = createStore(
+	reducers,
+	compose(
+		applyMiddleware(thunk),
+		window.__REDUX_DEVTOOLS_EXTENSION__
+			? window.__REDUX_DEVTOOLS_EXTENSION__()
+			: f => f
+	)
+);
 
 ReactDOM.render(
-  (<Provider store={store}>
-    <BrowserRouter>
-      <div>
-        <AuthRoute></AuthRoute>
-      </div>
-    </BrowserRouter>
-  </Provider>
-  ), document.getElementById('root')
-)
+	<Provider store={store}>
+		<BrowserRouter>
+			<div>
+				<AuthRoute></AuthRoute>
+			</div>
+		</BrowserRouter>
+	</Provider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
