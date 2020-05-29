83b68bb62fef2a3470754cb6f3b13955869be8f5
“mayinghan” <ma.yingha@ufl.edu>
2020-05-29 19:16:29 -0400

    Add verify email button to registration form

--- client/container/auth/Register.js
+++ client/container/auth/Register.js
@@ -1,32 +1,54 @@
 import React from 'react';
 import { UserOutlined } from '@ant-design/icons';
 import '@ant-design/compatible/assets/index.css';
-import { Form, Input, Typography, Button, Radio } from 'antd';
+import { Form, Input, Typography, Button, Radio, Row, Col, Tooltip } from 'antd';
 import { connect } from 'react-redux';
 import { register } from '../../redux/user.redux';
 import { Redirect } from 'react-router-dom';
 const { Paragraph } = Typography;
 
+const EMAIL_COOL_DOWN = 10;
 
 @connect(state => state.user, { register })
 class RegisterForm extends React.Component {
 	constructor (props) {
 		super(props);
 		this.state = {
-			confirmDirty: false
+			confirmDirty: false,
+			emailValid: false,
+			codeResendLoading: false,
+			codeResendCoolTime: EMAIL_COOL_DOWN
 		};
 		this.formRef = React.createRef();
 	}
 
-	handleSubmit (e) {
-		e.preventDefault();
-		this.props.form.validateFieldsAndScroll((err, values) => {
-			if (!err) {
-				this.props.register(values);
-			} else {
-				console.log(err);
-			}
-		});
+	handleEmailChange = (e) => {
+		const email = e.target.value;
+		if (/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.[a-zA-Z]{2,4}$/.test(email)) {
+			this.setState({ emailValid: true });
+		} else {
+			this.setState({ emailValid: false });
+		}
+	}
+
+	handleSendButton = () => {
+		const email = this.formRef.current.getFieldValue('email');
+		this.sendEmail(email);
+		this.setState({ codeResendLoading: true });
+		const interval = setInterval(() => {
+			this.setState((state) => {
+				if (state.codeResendCoolTime <= 0) {
+					clearInterval(interval);
+					return { codeResendLoading: false, codeResendCoolTime: EMAIL_COOL_DOWN };
+				} else {
+					return { codeResendCoolTime: state.codeResendCoolTime - 1 };
+				}
+			});
+		}, 1000);
+	}
+
+	sendEmail = (email) => {
+		console.log(email);
 	}
 
 	onFinish = values => {
@@ -36,6 +58,13 @@ class RegisterForm extends React.Component {
 	onFinishFailed = ({ errorFields }) => {
 		this.formRef.current.scrollToField(errorFields[0].name);
 	};
+
+	emailValidator = (rule, value) => {
+		if (!value || /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.[a-zA-Z]{2,4}$/.test(value)) {
+			return Promise.resolve();
+		}
+		return Promise.reject('Please enter a valid email address!');
+	}
 
 	phoneValidator = (rule, value) => {
 		if (
@@ -71,7 +100,6 @@ class RegisterForm extends React.Component {
 
 		return (
 			<div>
-				{console.log(this.props.redirectTo ? 'yes' : 'no')}
 				{this.props.redirectTo ? (
 					<Redirect to={this.props.redirectTo}></Redirect>
 				) : null}
@@ -136,17 +164,17 @@ class RegisterForm extends React.Component {
 							label='E-mail'
 							rules={[
 								{
-									type: 'email',
-									message: 'The input is not a valid E-mail address'
-								},
-								{
 									required: true,
 									message: 'Please input your E-mail!'
+								},
+								{
+									validator: this.emailValidator
 								}
 							]}
 						>
-							<Input />
+							<Input onChange={this.handleEmailChange}/>
 						</Form.Item>
+						
 						<Form.Item
 							name='pwd'
 							label='Password'
@@ -214,6 +242,44 @@ class RegisterForm extends React.Component {
 							]}
 						>
 							<Input type='String' placeholder='US Phone Number' />
+						</Form.Item>
+						<Form.Item label="Verify Email">
+							<Row gutter={8}>
+								<Col span={16}>
+									<Form.Item
+										name='code'
+										noStyle						
+										rules={[
+											{
+												required: true,
+												message: 'Please input the email verification code you got!'
+											}
+										]}
+									>
+										<Input />
+									</Form.Item>
+								</Col>
+								<Col span={6}>
+									{this.state.emailValid ? (
+										<Tooltip title='Click to get the verification code in your email'>
+											<Button
+												disabled={!this.state.emailValid || this.state.codeResendLoading}
+												onClick={this.handleSendButton}
+											>
+												{this.state.codeResendLoading
+													? this.state.codeResendCoolTime + 's'
+													: 'Get Code'}
+											</Button>
+										</Tooltip>
+									) : (
+										<Tooltip title='Enter a valid email address and get a verification code'>
+											<Button disabled={!this.state.emailValid} onClick={this.handleSendButton}>
+										Get Code
+											</Button>
+										</Tooltip>
+									)}
+								</Col>
+							</Row>
 						</Form.Item>
 						<Form.Item
 							wrapperCol={{
